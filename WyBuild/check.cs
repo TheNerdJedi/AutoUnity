04aa679c90df804c0105d892acfb701e81ff242f
Shajed Ahmed <shajedahmed@gmail.com>
2015-08-18 14:58:58 -0400

    Added correct Libraries

--- WyBuild/check.cs
+++ WyBuild/check.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections;
 
+using System;
+using System.Diagnostics;
+
 public class check : MonoBehaviour {
 
     // Variables
@@ -26,7 +29,26 @@ public class check : MonoBehaviour {
         if (GUI.Button(UpdateButton, "Check For Updates"))
         {
             // What happens if Check for Updates is clicked
-
+            
+            try
+            {
+                Process myProcess = new Process();
+                myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = "C:\\Windows\\system32\\cmd.exe";
+                string path = "C:\\Users\\Russell\\Desktop\\testFile.bat";
+                myProcess.StartInfo.Arguments = "/c" + path;
+                myProcess.EnableRaisingEvents = true;
+                myProcess.Start();
+                myProcess.WaitForExit();
+                int ExitCode = myProcess.ExitCode;
+                //print(ExitCode);
+            }
+            catch (Exception e)
+            {
+                print(e);
+            }
 
 
 
